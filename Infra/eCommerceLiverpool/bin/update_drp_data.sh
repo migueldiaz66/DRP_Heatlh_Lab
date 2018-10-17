fad54d257aab5fb9a2f460895a14ba2aa985da48
12Factor-mx <miguel.diaz@12factor.mx>
2018-10-17 10:53:04 -0500

    added the updateweblogicservers parent functionality

--- Infra/eCommerceLiverpool/bin/update_drp_data.sh
+++ Infra/eCommerceLiverpool/bin/update_drp_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ansible-playbook -i /home/mdiazm/DRP_Heatlh_Lab/Infra/eCommerceLiverpool/Vagrant/.vagrant/provisioners/ansible/inventory/ ../Ansible/playbooks/verify_weblogic_servers_health.yml
+ansible-playbook -i /home/mdiazm/DRP_Heatlh_Lab/Infra/eCommerceLiverpool/Vagrant/.vagrant/provisioners/ansible/inventory/ ../Ansible/playbooks/update_weblogic_servers_parents.yml
 
