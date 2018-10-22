5f7d022841e7a4d98917a3ed535642e597270d49
12Factor-mx <miguel.diaz@12factor.mx>
2018-10-21 23:00:45 -0500

    bux fixed

--- Infra/eCommerceLiverpool/bin/update_drp_data.sh
+++ Infra/eCommerceLiverpool/bin/update_drp_data.sh
@@ -2,4 +2,6 @@
 
 ansible-playbook -i /home/mdiazm/DRP_Heatlh_Lab/Infra/eCommerceLiverpool/Vagrant/.vagrant/provisioners/ansible/inventory/ ../Ansible/playbooks/verify_weblogic_servers_health.yml
 ansible-playbook -i /home/mdiazm/DRP_Heatlh_Lab/Infra/eCommerceLiverpool/Vagrant/.vagrant/provisioners/ansible/inventory/ ../Ansible/playbooks/update_weblogic_servers_parents.yml
+ansible-playbook -i /home/mdiazm/DRP_Heatlh_Lab/Infra/eCommerceLiverpool/Vagrant/.vagrant/provisioners/ansible/inventory/ ../Ansible/playbooks/update_weblogic_parents.yml
+ansible-playbook -i /home/mdiazm/DRP_Heatlh_Lab/Infra/eCommerceLiverpool/Vagrant/.vagrant/provisioners/ansible/inventory/ ../Ansible/playbooks/update_ecommerce_parents.yml
 
